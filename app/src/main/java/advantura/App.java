4eced44b8f5082423e51628959574c070ab1dbf3
Irfan Zainudin <m.irfan.zain@gmail.com>
2024-09-29 18:07:12 +0800

    satisfying the default test case

--- app/src/main/java/advantura/App.java
+++ app/src/main/java/advantura/App.java
@@ -4,17 +4,22 @@ package advantura;
 
 import javax.swing.JFrame;
 
-public class App
-{
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
         JFrame window = new JFrame();
         GamePanel gp = new GamePanel();
-        
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Advantura");
+        
         window.add(gp);
+
         window.pack();
+        
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
